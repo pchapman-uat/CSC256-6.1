3495b63cc010aa671d374c90ce8dd49aff74888b
Preston Chapman <pchapman82070@uat.edu>
2024-06-22 16:24:36 -0700

    Add start game

--- script/Character.js
+++ script/Character.js
@@ -1,11 +1,12 @@
 class Character {
-    constructor(name){
+    constructor(name, health){
         this.name = name;
+        this.health = health;
     }
 }
 class Player extends Character {
     constructor(username, name){
-        super(name);
+        super(name, 100);
         this.username = username;
         this.score = 0;
     }
--- script/game.js
+++ script/game.js
@@ -1,15 +1,38 @@
 import Player from "./Character.js";
 document.addEventListener('DOMContentLoaded',main);
+var player;
 
+const debug = true;
 function main(){
     console.log('Hello World');
     const REGISTER_FORM = document.getElementById("accountForm")
     REGISTER_FORM.addEventListener("submit", register)
+    if(debug == true){
+        player = new Player("Username", "Display Name");
+        player.score = 1000;
+        startGame();
+    }
 }
 function register(event){
     event.preventDefault();
     let username = document.getElementById("username").value;
     let displayName = document.getElementById("displayName").value;
-    let player = new Player(username, displayName);
+    player = new Player(username, displayName);
     console.log(player);
+    startGame();
+}
+
+function startGame(){
+    console.log('Game Started');
+    const PLAYER_STATS = {
+        displayName: document.getElementById("game_DisplayName"),
+        score: document.getElementById("game_Score"),
+        health: document.getElementById("game_Health"),
+    }
+
+    PLAYER_STATS.displayName.innerHTML = player.name;
+    PLAYER_STATS.score.innerHTML = "Score: "+player.score;
+    PLAYER_STATS.health.innerHTML = "Health: "+player.health;
+    
+
 }
